39b49c9243512810acc931fab2f66859aa58d73f
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-12-28 22:48:12 +0900

    Initial commit

---
+++ src/main/java/com/sideproject/linkplanet/LinkplanetApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.linkplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkplanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/linkplanet/LinkplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.linkplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
